a1b7ab7b670dc67e6bac03d8d52a1fc57a201aa5
Dans Bootcamp <dansbootcamp@AmbersImac.home>
2019-10-18 16:14:28 -0400

    server up and working

--- server.js
+++ server.js
@@ -4,17 +4,35 @@ var path = require("path");
 var app = express();
 var PORT = 3000;
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+
+// app.use(express.static("app/public"));
+
+// require("./routing/apiroutes.js")(app);
+// require("./routing/htmlroutes.js")(app);
 
 // Question data will probably go here
-var Questions = [
-    {
-        
-    }
-]
+// var Questions = [
+//     {
 
-app.get("/", function(req, res) {
-    // res.send("Welcome to the Star Wars Page!")
-    res.sendFile(path.join(__dirname, "view.html"));
+//     }
+// ]
+
+// app.get("/", function(req, res) {
+//     res.sendFile(path.join(__dirname, "home.html"));
+//   });
+
+
+
+
+
+
+
+
+
+
+  app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
   });
+  
